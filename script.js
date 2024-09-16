ce85ac645c39c5f15e8ef141ea07df9e105828c0
Boitumelo62 <boitumelomaphalala62@gmail.com>
2024-09-16 11:28:26 +0200

    AficanDiamond

---
+++ script.js
@@ -0,0 +1,58 @@
+// header script
+
+const header = document.querySelector("header");
+const logo = document.getElementById("logo");
+
+const fullLogo = "images/icons/main_logo.webp";
+const iconLogo = "images/icons/small_logo.webp";
+
+function checkScroll() {
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+    logo.src = iconLogo;
+  } else {
+    header.classList.remove("scrolled");
+    logo.src = fullLogo;
+  }
+}
+
+window.addEventListener("scroll", checkScroll);
+
+// cursor script
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", function (e) {
+  cursor.style.left = `${e.pageX}px`;
+  cursor.style.top = `${e.pageY}px`;
+});
+
+document.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("mouseenter", () => {
+    cursor.style.backgroundColor = "#dbdbdbbd";
+    // cursor.style.border = "2px solid #bb9339";
+    // cursor.style.scale = "2";
+    cursor.style.transform = "translate(-25%, -25%)";
+  });
+
+  link.addEventListener("mouseleave", () => {
+    cursor.style.backgroundColor = "#bb9339";
+    // cursor.style.border = "none";
+    // cursor.style.scale = "1";
+    cursor.style.transform = "translate(-50%, -50%)";
+  });
+});
+
+// gallery script
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Instagram Gallery Loaded.");
+});
+
+// carousel script
+
+$(document).ready(function () {
+  $("#carouselExampleText").carousel({
+    interval: 5000, // Change slide every 5 seconds
+  });
+});
